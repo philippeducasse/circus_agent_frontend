e5e73351f3b7a3fc0abd224b9d78e5d0b622308d
Philippe Ducasse <ducassephi@hotmail.fr>
2025-07-05 20:24:47 +0200

    added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,9 @@ import React, { useEffect } from "react";
 import useFestivals from "@/hooks/useFestivals";
 
 const Page = () => {
-  const festivals = useFestivals();
-  console.log("festivals:", festivals);
   return (
     <>
       <div>Circus Agent Frontend</div>
-      {festivals?.length > 0 ? (
-        <ul>
-          {festivals?.map((f) => (
-            <li key={f.id}>{f.festival_name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No festivals found!</p>
-      )}
     </>
   );
 };
--- src/hooks/useFestivals.tsx
+++ src/hooks/useFestivals.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Festival } from "@/interfaces/festival";
+import camelcaseKeys from "camelcase-keys";
 
 const useFestivals = () => {
-  const [festivals, setFestivals] = useState();
+  const [festivals, setFestivals] = useState<Festival[]>([]);
 
   useEffect(() => {
     const fetchFestivals = async () => {
       try {
         const data = await fetch("http://localhost:8000/api/festivals/");
-        const festivals = await data.json();
-
-        setFestivals(festivals);
+        const json = await data.json();
+        const camelized = camelcaseKeys(json, { deep: true });
+        if (festivals) setFestivals(camelized);
       } catch (error) {
         console.error(`Error fetching festivals: ${error}`);
       }
