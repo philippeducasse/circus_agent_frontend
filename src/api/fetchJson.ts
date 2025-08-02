9864c830862fd83b685d184012934d1e4bb78cd6
Philippe Ducasse <ducassephi@hotmail.fr>
2025-08-02 16:20:36 +0200

    disabled any rule for serialiser

--- src/api/fetchJson.ts
+++ src/api/fetchJson.ts
@@ -1,11 +1,10 @@
-import camelcaseKeys from "camelcase-keys";
-import snakecaseKeys from "snakecase-keys";
+import { transformKeysToCamelCase, transformKeysToSnakeCase } from "@/helpers/serializer";
 
 export async function fetchJson<T = unknown>(url: string, options?: RequestInit): Promise<T> {
   const res = await fetch(url, options);
   if (!res.ok) throw new Error(`Failed to fetch ${url}: ${res.status}`);
   const json = await res.json();
-  return camelcaseKeys(json, { deep: true });
+  return transformKeysToCamelCase(json);
 }
 
 export async function sendJson<T = unknown>(
@@ -19,14 +18,12 @@ export async function sendJson<T = unknown>(
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    body: JSON.stringify(snakecaseKeys(data, { deep: true })),
+    body: JSON.stringify(transformKeysToSnakeCase(data)),
   });
-
   if (!res.ok) {
     const error = await res.text();
     throw new Error(`Failed to ${method} ${url}: ${res.status} - ${error}`);
   }
-
   const json = await res.json();
   return json;
 }
---
+++ src/helpers/serializer.ts
@@ -0,0 +1,27 @@
+import { camelCase, snakeCase } from "lodash";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const transformKeysToCamelCase = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => transformKeysToCamelCase(v));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce((result, key) => {
+      const camelKey = camelCase(key);
+      result[camelKey] = transformKeysToCamelCase(obj[key]);
+      return result;
+    }, {} as any);
+  }
+  return obj;
+};
+
+export const transformKeysToSnakeCase = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => transformKeysToSnakeCase(v));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce((result, key) => {
+      const snakeKey = snakeCase(key);
+      result[snakeKey] = transformKeysToSnakeCase(obj[key]);
+      return result;
+    }, {} as any);
+  }
+  return obj;
+};
