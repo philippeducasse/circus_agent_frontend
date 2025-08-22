290cd4ba62f8b0e7573635af5656adad6499bcb7
Philippe Ducasse <ducassephi@hotmail.fr>
2025-08-22 09:30:28 +0200

    adding redux, wip

--- src/app/festivals/page.tsx
+++ src/app/festivals/page.tsx
@@ -1,34 +1,11 @@
-"use client";
-
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "@/redux/store";
-import { selectAllFestivals, selectFestivalsStatus, fetchFestivals } from "@/redux/slices/festivalSlice";
+import festivalApiService from "@/api/festivalApiService";
 import { FestivalTable } from "../../components/page-components/festival-page/components/view/FestivalTable";
-import { DynamicProgress } from "@/components/common/DynamicProgress";
 
-const FestivalsPage = () => {
-  const dispatch: AppDispatch = useDispatch();
-  const festivals = useSelector(selectAllFestivals);
-  const status = useSelector(selectFestivalsStatus);
-
-  useEffect(() => {
-    if (status === "idle") {
-      dispatch(fetchFestivals());
-    }
-  }, [status, dispatch]);
-
-  if (status === "loading" || status === "idle") {
-    return <DynamicProgress />;
-  }
-
-  if (status === "failed") {
-    return <div>Error loading festivals.</div>;
-  }
-
+const FestivalsPage = async () => {
+  const data = await festivalApiService.getAllFestivals();
   return (
     <div className="container mx-auto py-10">
-      <FestivalTable data={festivals} />
+      <FestivalTable festivals={data} />
     </div>
   );
 };
--- src/redux/StoreProvider.tsx
+++ src/redux/StoreProvider.tsx
@@ -3,6 +3,7 @@
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
+const StoreProvider = ({ children }: { children: React.ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
-}
+};
+export default StoreProvider;
